19a4eff9ea52c903e2d7e2ad7a1456d95400c30b
webberig <mathieu@webberig.be>
2014-07-06 13:45:19 +0000

    Init composer, add first Extension

---
+++ src/GeneralExtension.php
@@ -0,0 +1,15 @@
+<?php
+namespace Webberig\HtmlUtilbelt\Twig;
+/**
+ * Description of newPHPClass
+ *
+ * @author webberig
+ */
+class GeneralExtension extends \Twig_Extension, Webberig\HtmlUtilbelt\Generator\General {
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('abbr', array($this, "abbr"));
+        );
+    }
+}
